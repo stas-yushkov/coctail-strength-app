203c65b6708d2ae1cb36403b7848a23a723b967c
stas-yushkov <stas.yushkov@gmail.com>
2022-11-12 02:19:32 +0200

    add template files

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { colors } from 'constants/colors';
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: colors.color,
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
